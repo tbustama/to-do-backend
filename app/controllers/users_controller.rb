20d58926f25ae8ee562eba2499e7d216a781606f
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-25 17:04:45 -0800

    created the show action in our user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,27 @@
 class UsersController < ApplicationController
     def show
-        user = User.find_by(id: params["id"])
-        byebug
+        user = User.find_by(username: params[:id])
         render json: user.to_json(:include => {
             :tasks => {:except => [:created_at, :updated_at]},
             :notes => {:except => [:created_at, :updated_at]}},
             :except => [:updated_at, :created_at])
     end
+    
+    # def index 
+    #     byebug
+    #     users = User.all
+    #     render json: users.to_json(:include => {
+    #         :tasks => {:except => [:created_at, :updated_at]},
+    #         :notes => {:except => [:created_at, :updated_at]}},
+    #         :except => [:updated_at, :created_at])
+    # end
 
     def create
-        user = User.create(username: params["username"])
+        user = User.create(username: params[:user][:username])
         render json: user.to_json(:include => {
             :tasks => {:except => [:created_at, :updated_at]},
-            :notes => {:except => [:created_at, :updated_at]},
-            :except => [:updated_at, :created_at]})
+            :notes => {:except => [:created_at, :updated_at]}},
+            :except => [:updated_at, :created_at])
     end
 
     def destroy
