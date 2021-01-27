0657136006ef8e84a44456fad8fefc8fb7db14b1
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-27 08:26:22 -0800

    changed models in the backend

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,22 @@
 class TasksController < ApplicationController
     def create
-        task = Task.create(params)
+        time = params[:task][:due_date].split('-')
+        formatted_time = [time[2],time[0],time[1]].join("-")
+        
+        task = Task.create(title: params[:task][:title], description: params[:task][:description], due_date: formatted_time, priority_level: params[:task][:priority_level], user_id: params[:task][:user_id], duration: params[:task][:duration])
         render json: task
     end
 
     def update
-        task = Task.update(params)
+        task = Task.find_by(id: params[:id])
+        time = params[:task][:due_date].split('-')
+        formatted_time = [time[2],time[0],time[1]].join("-")
+        task.update(title: params[:task][:title], description: params[:task][:description], due_date: formatted_time, priority_level: params[:task][:priority_level], duration: params[:task][:duration], user_id: params[:task][:user_id])
+        render json: task
     end
 
     def destroy
         Task.find_by(id: params["id"]).destroy
     end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
   belongs_to :user
+  validates_presence_of :due_date, :title, :description
 end
