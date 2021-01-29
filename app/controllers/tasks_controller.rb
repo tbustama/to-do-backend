52e159815876016c383c1c115abb2a7fb3d6a008
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-28 18:41:23 -0800

    final commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,12 @@ class TasksController < ApplicationController
         formatted_time = [time[2],time[0],time[1]].join("-")
         
         task = Task.create(title: params[:task][:title], description: params[:task][:description], due_date: formatted_time, priority_level: params[:task][:priority_level], user_id: params[:task][:user_id], duration: params[:task][:duration])
-        render json: task
+        render json: task.to_json(:include =>{
+            :user => {:include => {
+                :tasks => {:except => [:created_at, :updated_at]},
+                :notes => {:except => [:created_at, :updated_at]}},
+                :except => [:updated_at, :created_at]}
+        })
     end
 
     def update
@@ -12,7 +17,13 @@ class TasksController < ApplicationController
         time = params[:task][:due_date].split('-')
         formatted_time = [time[2],time[0],time[1]].join("-")
         task.update(title: params[:task][:title], description: params[:task][:description], due_date: formatted_time, priority_level: params[:task][:priority_level], duration: params[:task][:duration], user_id: params[:task][:user_id])
-        render json: task
+        render json: task.to_json(:include => {
+            :user => {:include => {
+                :tasks => {:except => [:created_at, :updated_at]},
+                :notes => {:except => [:created_at, :updated_at]}},
+                :except => [:updated_at, :created_at]}
+        })
+       
     end
 
     def destroy
